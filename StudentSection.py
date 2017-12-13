c9134e6ca9402b2a64f44007c228d5a84c8aba93
Versumm <34036265+Versumm@users.noreply.github.com>
2017-12-13 19:17:34 +0300

    Update StudentSection.py

--- StudentSection.py
+++ StudentSection.py
@@ -5,64 +5,65 @@ import csv
 connectDatabase = connect("cms.db")
 db_cursor = connectDatabase.cursor()
 
+
 class grade:
 
-    def __init__(self,name,percentage,courseName):
-        self.name=name
-        self.courseName=courseName
-        self.percentage=percentage
+    def __init__(self, name, percentage, courseName,semesterName):
+        self.name = name
+        self.courseName = courseName
+        self.percentage = percentage
+        self.semesterName=semesterName
+        
 
-    def setGradeName(self,name):
-        self.name=name
+    def setGradeName(self, name):
+        self.name = name
 
-    def setCourseName(self,name):
-        self.courseName=name
+    def setCourseName(self, name):
+        self.courseName = name
 
-    def setPercentage(self,percentage):
-        self.percentage=percentage
-
+    def setPercentage(self, percentage):
+        self.percentage = percentage
 
 
 class Section:
-    def __init__(self,Name="Empty",day="Empty",hour="Empty",room="Empty"):
+    def __init__(self, Name="Empty", day="Empty", hour="Empty", room="Empty"):
         self.sectionID = None
-        self.sectionName=Name
-        self.day=day
-        self.hour=hour
-        self.classRoom=room
+        self.sectionName = Name
+        self.day = day
+        self.hour = hour
+        self.classRoom = room
 
-    def setSectionCode(self,code):
-        self.sectionCode=code
+    def setSectionCode(self, code):
+        self.sectionCode = code
 
     def getSectionCode(self):
         return self.sectionCode
 
-    def setDay(self,day):
-        self.day.append(day)
+    def setDay(self, day):
+        self.day=day
 
     def getDay(self):
         return self.day
 
-    def setHour(self,hour):
-        self.hour.append(hour)
+    def setHour(self, hour):
+        self.hour=hour
 
     def getHour(self):
         return self.hour
 
-    def setClassRoom(self,classRoom):
-        self.classRoom=classRoom
+    def setClassRoom(self, classRoom):
+        self.classRoom = classRoom
 
     def getClassRoom(self):
         return self.classRoom
 
-    def insertSectionInDatabase(self,semID,courseID):
-        self.sectionID = db_fonk.insert_section(self.sectionName,self.classRoom,self.hour,self.day)
-        db_fonk.insert_section_in(semID,courseID,sectionID)
+    def insertSectionInDatabase(self, semID, courseID):
+        self.sectionID = db_fonk.insert_section(self.sectionName, self.classRoom, self.hour, self.day)
+        db_fonk.insert_sectionin(semID, courseID, self.sectionID)
         db_fonk.save_changes()
 
-
-    def importStudent(self):
-        tempStudents=[]
+    def importStudent(self,semID,courseID):
+        tempStudents = []
         file = open('students.csv', "r")
         read = csv.reader(file)
         for row in read:
@@ -72,78 +73,70 @@ class Section:
                 a = tuple(a)
                 tempStudents.append(a)
         for x in tempStudents:
-            db_fonk.insert_student(x[0],x[1])
-
+            db_fonk.insert_student(x[0], x[1])
+            db_fonk.insert_studentin(semID,courseID,self.sectionID,x[1])
+            db_fonk.save_changes()
 
 
     def displaySection(self):
-        print("Section Code:",self.getSectionCode())
+        print("Section Code:", self.getSectionCode())
         print("Section Day/s:")
         for i in self.getDay():
             print(i)
         print("Hours:")
         for i in self.getHour():
             print(i)
-        print("Classroom:",self.getClassRoom())
-
-
-
+        print("Classroom:", self.getClassRoom())
 
 
 class Student():
-    def __init__(self,sName,sId):
-        self.studentName=sName
-        self.studentId=sId
-        self.note=[]
-        self.grade=[]
+    def __init__(self, sName, sId):
+        self.studentName = sName
+        self.studentId = sId
+        self.note = []
+        self.grade = []
 
-    def setStudentName(self,name):
-        self.studentName=name
+    def setStudentName(self, name):
+        self.studentName = name
 
     def getStudentName(self):
         return self.studentName
 
-    def setStudentId(self,id):
-        self.studentId=id
+    def setStudentId(self, id):
+        self.studentId = id
 
     def getStudentId(self):
         return self.studentId
 
-    def setNote(self,note):
+    def setNote(self, note):
         self.note.append(note)
 
     def getNote(self):
         return self.note
 
-    def setGrade(self,grade):
-        self.grade=grade
+    def setGrade(self, grade):
+        self.grade = grade
 
     def getGrade(self):
         return self.grade
 
     def getTotalGrade(self):
         pass
+
     def getLetterGrade(self):
         pass
 
     def insertStudentInDatabase(self):
-        db_fonk.insert_student(self.studentId,self.studentName)
-
+        db_fonk.insert_student(self.studentId, self.studentName)
+        db_fonk.save_changes()
+        
 
     def display(self):
-        print("Name:",self.getStudentName())
-        print("Id:",self.getStudentId())
+        print("Name:", self.getStudentName())
+        print("Id:", self.getStudentId())
         print("Notes:")
-        counter=1
+        counter = 1
         for i in self.getNote():
-            print(counter,":",i)
-            counter=counter+1
-            
-            
-            
-  
-
-
-
-
+            print(counter, ":", i)
+            counter = counter + 1
 
