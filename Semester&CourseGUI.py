9ec29f7fa0ac6409ad30cecd3f66f9c1fe7cfbbf
burak456 <32981043+burak456@users.noreply.github.com>
2017-12-17 10:30:38 +0300

    Update Semester&CourseGUI.py

--- Semester&CourseGUI.py
+++ Semester&CourseGUI.py
@@ -10,9 +10,14 @@ menubar = Menu(root)
 #MainButtonFrame.pack_propagate(0)
 #MainButtonFrame.pack(fill=BOTH, side= TOP)
 
+
 TestFrame = Frame(width=300, height=500, bg='white')
 TestFrame.pack_propagate(0)
 TestFrame.pack(fill=BOTH,expand=TRUE)
+
+TestFrame2 = Frame(width=300, height=500, bg='white')
+TestFrame2.pack_propagate(0)
+TestFrame2.pack(fill=BOTH,expand=TRUE)
 
 FirstFrame = Frame(width=300, height=500, bg='white')
 
@@ -22,105 +27,210 @@ HTMLFrame = Frame(width=450, height=50,bg='white')
 HTMLFrame.pack_propagate(0)
 HTMLFrame.pack(fill=BOTH,side= BOTTOM)
 
+def center_window(width=300, height=200):
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
 
-def Create():
+    # calculate position x and y coordinates
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    root.geometry('%dx%d+%d+%d' % (width, height,x, y))
 
-    CreateButton.destroy()
+def SemTlevelPos(width=300, height=200):
+    # get screen width and height
+    screen_width = CreateSemTlevel.winfo_screenwidth()
+    screen_height = CreateSemTlevel.winfo_screenheight()
 
-    CreateCourseButton.place(relx=.285, rely=.7, anchor="center", height=50, width=95)
-    CreateSemesterButton.place(relx=.5, rely=.7, anchor="center", height=50, width=95)
+    # calculate position x and y coordinates
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    CreateSemTlevel.geometry('%dx%d+%d+%d' % (width, height,250,y))
 
+def CourseTlevelPos(width=300, height=200):
+    # get screen width and height
+    screen_width = CreateCoTlevel.winfo_screenwidth()
+    screen_height = CreateCoTlevel.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    CreateCoTlevel.geometry('%dx%d+%d+%d' % (width, height,x, y))
+def setActive(self):
+        self.lift()
+        self.focus_force()
+        self.grab_set()
+        self.grab_release()
+
+def Buttons():#MainButtons{
+    global Semester,Course,HTMLButton
+
+    Semester = Button(root, text='Add/Select a Semester',command = Semesters)
+    #Course = Button(root,text='Course',command = Courses)
+
+    HTMLButton = Button(root,text='HTML File')
+    #}
+
+def Activee(x):
+    x.config(state='disabled')
+def Diactive(x):
+    x.config(state='normal')
+
+#Semester Functions{
+def CreateSemTlevell():
+    global CreateSemTlevel
+    CreateSemTlevel = Toplevel(width = 450,height= 580,bg='white')
+def AddNewSmesterBttn():
+    global AddNewSemesterButton
+    AddNewSemesterButton = Button(CreateSemTlevel,text= 'Add Semester',command=AddingSemFunc)
+def SemesterEntrys():
+    global SemNameEntry,WeekEntry
+    SemNameEntry=Entry(CreateSemTlevel)
+    WeekEntry=Entry(CreateSemTlevel)
+def AddingSemFunc():
+    SemesterListInsert()
+    SemesterList.place(relx=.1,rely=.25,height=250, width=200)
+    CreateSemTlevel.destroy()
+def SemesterListBox():
+    global SemesterList
+    SemesterList = Listbox(CreateSemTlevel)
+def SemesterListInsert():
+    SemesterList.insert(0, " "+ SemNameEntry.get())
+# }
+
+#Course Functions{
+def CreateNewCoTlevell():
+    global CreateCoTlevel
+    CreateCoTlevel = Toplevel(width=450, height=580, bg='white')
+def AddNewCourseBttn():
+    global AddNewCourseButton
+    AddNewCourseButton = Button(CreateCoTlevel,text= 'Add Course',command=AddingCourseFunc)
+def CourseEntrys():
+    global CoNameEntry,CoCodeEntry,CoBookEntry,CoRefBookEntry,SyllabusEntry
+    CoNameEntry=Entry(CreateCoTlevel)
+    CoCodeEntry=Entry(CreateCoTlevel)
+    CoBookEntry=Entry(CreateCoTlevel)
+    CoRefBookEntry=Entry(CreateCoTlevel)
+    SyllabusEntry=Entry(CreateCoTlevel)
+def CourseListBox():
+    global CourseList
+    CourseList = Listbox(CreateCoTlevel)
+def AddingCourseFunc():
+    CourseListInsert()
+    CourseList.place(relx=.1,rely=.25,height=250, width=200)
+    CreateCoTlevel.destroy()
+def CourseListInsert():
+    CourseList.insert(0, "" +CoCodeEntry.get())
+#}
 
 def CallCreateNewCourse():
 
-   toplevel = Toplevel(width=450, height=580, bg='white')
+   CreateNewCoTlevell()
+   CreateCoTlevel.grab_set()
+   CourseEntrys()
 
-   S1 = Label(toplevel, text ="Course Name :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
+   CoNameLabel = Label(CreateCoTlevel, text ="Course Name :")
+   CoNameLabel.pack(padx=350,pady=20)
 
-   S1 = Label(toplevel, text ="Course Code :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
+   CoNameEntry.pack(padx=350,pady=20)
 
-   S1 = Label(toplevel, text ="Course Book :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
+   CoCodeLabel = Label(CreateCoTlevel, text ="Course Code :")
+   CoCodeLabel .pack(padx=350,pady=20)
 
-   S1 = Label(toplevel, text ="Course Reference Book :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
+   CoCodeEntry.pack(padx=350,pady=20)
 
-   S1 = Label(toplevel, text ="Syllabus :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
+   CoBookLabel = Label(CreateCoTlevel, text ="Course Book :")
+   CoBookLabel.pack(padx=350,pady=20)
 
-   AddCourseButton = Button(toplevel,text= 'Add Course')
-   AddCourseButton.pack(side=BOTTOM,padx=350,pady=40)
+   CoBookEntry.pack(padx=350,pady=20)
 
-   toplevel.resizable(0,0)
+   CoRefBookLabel = Label(CreateCoTlevel, text ="Course Reference Book :")
+   CoRefBookLabel.pack(padx=350,pady=20)
 
+   CoRefBookEntry.pack(padx=350,pady=20)
 
+   SyllabusLabel = Label(CreateCoTlevel, text ="Syllabus :")
+   SyllabusLabel.pack(padx=350,pady=20)
+
+   SyllabusEntry.pack(padx=350,pady=20)
+
+   AddNewCourseBttn()
+   AddNewCourseButton.pack(side=BOTTOM,padx=350,pady=40)
+
+   CreateCoTlevel.resizable(0,0)
 
 def CallCreateNewSemester():
 
-   toplevel = Toplevel(width=450, height=580, bg='white')
+   CreateSemTlevell()
+   CreateSemTlevel.grab_set()
+   SemesterEntrys()
+
+   SemNameLabel = Label(CreateSemTlevel, text ="*Semester Name : ",font='Arial')
+   SemNameLabel.pack(padx=350,pady=20)
+
+   SemNameEntry.pack(padx=350,pady=20)
+
+   WeekLabel = Label(CreateSemTlevel, text ="*Week :" ,font='Arial')
+   WeekLabel.pack(padx=350,pady=20)
+
+   WeekEntry.pack(padx=350,pady=20)
+   CreateSemTlevel.resizable(0,0)
+
+   AddNewSmesterBttn()
+   AddNewSemesterButton.pack(side=BOTTOM,padx=350,pady=40)
 
 
-   S1 = Label(toplevel, text ="Semester Name :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
-
-   S1 = Label(toplevel, text ="Week :")
-   S1.pack(padx=350,pady=20)
-   SeCode=Entry(toplevel)
-   SeCode.pack(padx=350,pady=20)
-   toplevel.resizable(0,0)
-
-   AddSemesterButton = Button(toplevel,text= 'Add Semester')
-   AddSemesterButton.pack(side=BOTTOM,padx=350,pady=40)
-
-
-
-
-
-
-
-def CreateSemesters():
-     toplevel = Toplevel(width = 450,height= 580,bg='white')
+def Semesters():
 
      #FirstFrame.pack_propagate(0)
      #FirstFrame.place( anchor="e", relx=.5, rely=.63)
 
+     CreateSemTlevell()
+     CreateSemTlevel.grab_set()
+     SemTlevelPos(450,580)
+     SemesterListBox()
 
-     CreatedSemesters = Text(toplevel,borderwidth=0)
+     CreatedSemesters = Text(CreateSemTlevel,borderwidth=0,font='Arial')
      CreatedSemesters.insert(INSERT,"Created Semesters")
      CreatedSemesters.place(relx=.1,rely=.18,height=30, width=150)
 
-     Lb1 = Listbox(toplevel)
-     Lb1.insert(1, "")
-     Lb1.insert(2, "")
-     Lb1.insert(3, "")
-     Lb1.insert(4, "")
-     Lb1.insert(5, "")
-     Lb1.insert(6, "")
-     Lb1.place(relx=.1,rely=.25,height=250, width=200)
 
-     Add = Button(toplevel,text='Add Semester ',command = CallCreateNewSemester)
-     Add.place(relx=.8, rely=.5, anchor="center", height=35, width=95)
+     Add = Button(CreateSemTlevel,text='Add Semester ',command = CallCreateNewSemester)
+     Add.place(relx=.8, rely=.5, anchor="center", height=40, width=95)
 
-     Add = Button(toplevel,text='Select Semester ')
-     Add.place(relx=.8, rely=.4, anchor="center", height=35, width=95)
+     SelectSem = Button(CreateSemTlevel,text='Select Semester',command= Courses)
+     SelectSem.place(relx=.8, rely=.4, anchor="center", height=40, width=95)
+
+
 
 
 
 def Courses():
+
+     CreateNewCoTlevell()
+     CreateCoTlevel.grab_set()
+     CourseTlevelPos(450,580)
+     CourseListBox()
+
+     #SecondFrame.pack_propagate(0)
+     #SecondFrame.place( anchor="w", relx=.5, rely=.63)
+
+     CreatedCourses = Text(CreateCoTlevel,borderwidth=0,font='Arial')
+     CreatedCourses.insert(INSERT,"Created Courses")
+     CreatedCourses.place(relx=.1,rely=.18,height=30, width=150)
+
+
+     Add = Button(CreateCoTlevel,text='Add Course',command=CallCreateNewCourse)
+     Add.place(relx=.8, rely=.5, anchor="center", height=40, width=95)
+
+     Add = Button(CreateCoTlevel,text='Select Course')
+     Add.place(relx=.8, rely=.4, anchor="center", height=40, width=95)
+
+
+
+
+
+def Section():
      toplevel = Toplevel(width=450,height=580,bg='white')
 
      #SecondFrame.pack_propagate(0)
@@ -139,10 +249,10 @@ def Courses():
      Lb1.insert(6, "")
      Lb1.place(relx=.1,rely=.25,height=250, width=200)
 
-     Add = Button(toplevel,text='Add Course',command=CallCreateNewCourse)
+     Add = Button(toplevel,text='Add Section',command=CallCreateNewCourse)
      Add.place(relx=.8, rely=.5, anchor="center", height=35, width=95)
 
-     Add = Button(toplevel,text='Select Course')
+     Add = Button(toplevel,text='Select Section')
      Add.place(relx=.8, rely=.4, anchor="center", height=35, width=95)
      toplevel.resizable(0,0)
 
@@ -150,14 +260,11 @@ def Courses():
 
 #Main
 
+Buttons()
 
-Semester = Button(root, text='Semester',command = CreateSemesters)
-Semester.place(relx=.5, rely=.4, anchor="center", height=50, width=95)
+Semester.place(relx=.5, rely=.45, anchor="center", height=50, width=140)
+#Course.place(relx=.5, rely=.5, anchor="center", height=50, width=95)
 
-Course = Button(root,text='Course',command = Courses)
-Course.place(relx=.5, rely=.5, anchor="center", height=50, width=95)
-
-HTMLButton = Button(root,text='HTML File')
 HTMLButton.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE,height=50,width=95)
 
 filemenu = Menu(menubar, tearoff=0)
@@ -167,9 +274,11 @@ filemenu.add_command(label="Exit", command=root.quit)
 helpmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
+
 root.winfo_x()
 root.winfo_y()
 root.config(menu=menubar)
+center_window(400, 500)
 #root.resizable(0,0)
 root.mainloop()
 
