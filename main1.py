8ae89b2ef90a1618b7a32839e3de35038706849c
yilmazumut <32803270+yilmazumut@users.noreply.github.com>
2017-12-23 05:44:21 +0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,18 +4,8 @@ from semester_class import*
 from course_class import*
 from student_section import*
 
-
-
-
 create_entity_tables()
 create_relation_tables()
 
-section = Section()
-announcement = Announcement()
-subject = Subject()
-grade = Grade()
-student = Student()
-notee = Note()
+
 from NewGUI import *
-
-
